ec1fc3a9f666f21f5bc91d51f914c2a8cf831bee
JAVIER32456 <ronaltomas932@gmail.com>
2024-08-30 23:16:14 -0500

    segundo cambio

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hola ha iniado un nuevo proyecto
         </p>
       </div>
       <p className="read-the-docs">
